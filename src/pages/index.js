15d0e8a19441cc8bdb1a83a309536a4e0a77ed65
Muhammad Yazid Akbar <yazidakbar08@gmail.com>
2023-08-26 21:20:20 +0700

    finish

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,146 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+/* eslint-disable @next/next/no-img-element */
+import Spline from "@splinetool/react-spline";
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalContent,
+  Progress,
+} from "@nextui-org/react";
+import { useEffect, useState } from "react";
+import confetti from "canvas-confetti";
+import Link from "next/link";
+import { NextSeo } from "next-seo";
 
 export default function Home() {
+  const [open, setOpen] = useState(false);
+  const [openFinished, setOpenFinished] = useState(false);
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    let interval;
+
+    if (open) {
+      interval = setInterval(() => {
+        setValue((v) => {
+          if (v >= 100) {
+            clearInterval(interval);
+            setOpen(false);
+            setOpenFinished(true);
+            confetti({
+              particleCount: 100,
+              spread: 70,
+              origin: { y: 0.6 },
+            });
+            return 0;
+          } else {
+            return v + 1;
+          }
+        });
+      }, 100);
+    }
+
+    return () => clearInterval(interval);
+  }, [open]);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="bg-[#FCEBED] h-screen w-screen">
+      <NextSeo
+        title="Yazz | Make Everything OK"
+        description="Semoga harimu menyenangkan :3"
+        canonical="https://yazz-make-everything-ok.vercel.app"
+        openGraph={{
+          url: "https://yazz-make-everything-ok.vercel.app",
+          images: [
+            {
+              url: "https://ih1.redbubble.net/image.4822587175.7657/st,small,507x507-pad,600x600,f8f8f8.jpg",
+              width: 600,
+              height: 600,
+              alt: "Follow my instagram @yazz_803",
+            },
+          ],
+        }}
+      />
+      <div className="absolute z-50 right-5 top-5">
+        <Link href="https://www.instagram.com/yazz_803/" target="_blank">
+          <img
+            src="/assets/ic-instagram2.svg"
+            alt="icon instagram"
+            width={50}
+            height={50}
+          />
+        </Link>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="absolute z-50 top-[72%] right-[50%] translate-x-[50%] translate-y-[50%]">
+        <Button
+          onPress={() => setOpen(true)}
+          size="lg"
+          radius="full"
+          className="bg-pink-300/20 backdrop-blur-sm text-slate-500 text-xl font-bold shadow-md shadow-pink-300 border-2 border-pink-300 active:scale-90 transition duration-150 ease-in-out"
+        >
+          Make Everything OK
+        </Button>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <Modal
+        isOpen={open}
+        onOpenChange={() => {}}
+        hideCloseButton
+        onClose={() => setOpen(false)}
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalBody>
+                <p className="text-center font-semibold text-xl mb-4">
+                  Making everything OK is in progress
+                </p>
+                <Progress
+                  aria-label="Checking..."
+                  size="md"
+                  value={value}
+                  color="danger"
+                  showValueLabel={true}
+                  label="Checking..."
+                  classNames={{
+                    base: "max-w-md",
+                    track: "drop-shadow-md border border-default",
+                    indicator: "bg-gradient-to-r from-pink-200 to-pink-400",
+                    label: "tracking-wider font-medium text-default-600",
+                    value: "text-foreground/60 text-center",
+                  }}
+                />
+              </ModalBody>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+      <Modal isOpen={openFinished} onOpenChange={() => {}} hideCloseButton>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalBody className="text-center">
+                <p className="font-semibold text-xl mb-4">
+                  Everything is OK now :3
+                </p>
+                <p>
+                  If everything is still not OK, try checking your settings of
+                  perception of objective reality.
+                </p>
+                <Button
+                  onPress={() => setOpenFinished(false)}
+                  radius="full"
+                  className="my-3 bg-pink-300/20 backdrop-blur-sm text-slate-500 text-xl font-bold shadow-md shadow-pink-300 border-2 border-pink-300 active:scale-90 transition duration-150 ease-in-out"
+                >
+                  Continue
+                </Button>
+              </ModalBody>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+      <div className="fixed w-full h-full">
+        <Spline scene="https://prod.spline.design/plZM6Wt5CVnYFcwQ/scene.splinecode" />
       </div>
-    </main>
-  )
+    </div>
+  );
 }
